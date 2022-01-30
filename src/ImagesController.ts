af2c515ba48fa8266a73e99dd61d101244a7a183
Matthias Martin <martin@zoom7.de>
2022-01-30 13:05:59 +0100

    4 crawlers and ignore duplicate errors

--- src/ImagesController.ts
+++ src/ImagesController.ts
@@ -5,13 +5,12 @@ export const storeImage = async (image: ImageType) => {
   try {
     const client = await pool.connect();
 
-    const sql = `INSERT INTO images(id, party, date, country, city, url) VALUES (${image.id}, '${image.party}', '${image.date}', '${image.country}', '${image.city}', '${image.url}')`;
+    const sql = `INSERT INTO images(id, party, date, country, city, url) VALUES (${image.id}, '${image.party}', '${image.date}', '${image.country}', '${image.city}', '${image.url}') ON CONFLICT DO NOTHING`;
     await client.query(sql);
 
     client.release();
     return true;
   } catch (error) {
-    console.error(error);
     return false;
   }
 };
