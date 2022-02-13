79c5e30c3620f4bc62770bbc4799a57e87242a76
Matthias Martin <martin@zoom7.de>
2022-02-13 09:37:06 +0100

    cookie problem

--- app/app/cookies.ts
+++ app/app/cookies.ts
@@ -1,7 +1,7 @@
 import { createCookie } from 'remix';
 
 export const authCookie = createCookie('auth', {
-  domain: process.env.NODE_ENV === 'production' ? process.env.APP_BASE_URL : undefined,
+  // domain: process.env.NODE_ENV === 'production' ? process.env.APP_BASE_URL : undefined,
   expires: new Date(Date.now() + 60_000 * 60 * 24 * 365), // 1 year
   httpOnly: true,
   maxAge: 60 * 60 * 24 * 365, // 1 year
