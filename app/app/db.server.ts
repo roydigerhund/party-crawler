12ad23578197b81f1ebdfe99e4ca05b9b392766e
Matthias Martin <github@stichwort-m.de>
2022-02-28 22:24:42 +0100

    different database connection code

--- app/app/db.server.ts
+++ app/app/db.server.ts
@@ -1,19 +1,23 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-let db: PrismaClient<
-  Prisma.PrismaClientOptions,
-  never,
-  Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
->;
+let db: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
-  db = new PrismaClient();
-} else {
-  if (!(global as any).db) {
-    (global as any).db = new PrismaClient();
-  }
-
-  db = (global as any).db;
+declare global {
+  var __db: PrismaClient | undefined;
 }
 
-export default db;
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+  db.$connect();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+    global.__db.$connect();
+  }
+  db = global.__db;
+}
+
+export default db
